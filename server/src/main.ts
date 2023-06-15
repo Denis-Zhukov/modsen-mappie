c8be31e1e58f4161344462c6e1c93f822221565d
Denis-Zhukov <DenisZhukov.Hachiko@gmail.com>
2023-06-15 08:59:49 +0300

    origin all added on main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://guide-modsen.vercel.app',
+    origin: '*',
     credentials: true
   });
   app.useGlobalPipes(new ValidationPipe());
