07314b7216bef2df097024232dea068d97887ba0
Denis-Zhukov <DenisZhukov.Hachiko@gmail.com>
2023-06-15 08:50:00 +0300

    Fixed origin. NEED TO ADD ENV

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*'
+    origin: 'https://guide-modsen.vercel.app',
+    credentials: true
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8000);
