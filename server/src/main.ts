b22b8fa121713d68e67eab181f41b8bf6669d45d
Denis-Zhukov <DenisZhukov.Hachiko@gmail.com>
2023-06-20 03:43:40 +0300

    change origin

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://guide-modsen.vercel.app'],
+    origin: ['http://localhost:3000', 'https://guide-modsen.vercel.app'],
     credentials: true
   });
   app.useGlobalPipes(new ValidationPipe());
